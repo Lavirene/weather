038d656c07991b33a36ee0c1cd774cbd385b2b7e
Lavirene <108747106+Lavirene@users.noreply.github.com>
2022-09-04 18:09:15 +0300

    Footer project link change

--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <p>
-      <a href="https://github.com/Lavirene/App" target="_blank" rel="noreferrer">
+      <a href="https://github.com/Lavirene/weather" target="_blank" rel="noreferrer">
         Open-source code
       </a>{" "}
        by Lavirene
